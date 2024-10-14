e1ae335436c2a0784c39d78ba55e787dfb2de2d6
Ivan Shokhodko <pnupplol@gmail.com>
2024-10-14 16:09:36 +0300

    Homework_1_done

---
+++ homework-1/src/item.py
@@ -0,0 +1,16 @@
+class Item:
+    pay_rate = 0
+    all = []
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price *= self.pay_rate
+        return self.price
+    
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import sys, pytest
+sys.path.insert(0, '../homework-1')
+from src.item import Item
+
+@pytest.fixture
+def item1():
+    return Item("Test Item", 100, 2)
+
+def test_item_init(item1):
+    assert item1.name == "Test Item"
+    assert item1.price == 100
+    assert item1.quantity == 2
+    assert item1 in Item.all
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200
+    
+def test_apply_discount(item1):
+    Item.pay_rate = 0.8
+    assert item1.apply_discount() == 80
+    
+if __name__ == '__main__':
+    pytest.main(['--cov=src', '--cov-report=term-missing', 'test_item.py'])
